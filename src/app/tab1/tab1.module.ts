15bfc9fe59afb97c4b0837a095b148b1c32cfaea
maddy <mfoletta@uci.edu>
2024-11-19 15:19:44 -0800

    first page

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { CardComponent } from '../card/card.component';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, CardComponent]
 })
 export class Tab1PageModule {}
